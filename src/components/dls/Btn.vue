d6c5da51ec92f634c5fb93cb4d28418b01a82524
Elham <hashemi.elham22@gmail.com>
2023-08-27 19:23:42 +0330

    feat: working on dls components

---
+++ src/components/dls/Btn.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app-btn" :class="`btn_${color}`">
+        <div v-if="icon" class="flex items-center justify-center">
+            <img :src="icon" :alt="icon" />
+        </div>
+        <div v-else>{{ text }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface IProps {
+    text?: string;
+    color?: 'white' | 'primary' | 'danger';
+    icon?: string;
+}
+const props = defineProps<IProps>()
+const text = computed(() => props.text || 'No Text')
+const color = computed(() => props.color || 'white')
+const icon = computed(() => props.icon || false)
+
+</script>
+
+<style lang="scss" scoped>
+@import url('@/assets/styles/dls/_btn.scss');
+</style>
