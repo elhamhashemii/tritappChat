1bce47b6d1cf7c05c190ddc72f39c57e849307d1
Elham <hashemi.elham22@gmail.com>
2023-08-27 17:59:40 +0330

    feat: avatar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="text-3xl text-center">
     TritAppChat
+    <Avatar :img="avatar3" size="lg" />
   </div>
 </template>
+
+<script setup lang="ts">
+import Avatar from '@/components/dls/Avatar.vue';
+const avatar3 = 'src/assets/img/avatar-2.png'
+</script>
---
+++ src/components/dls/Avatar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="avatar" :class="size">
+       <img :src="img" alt="Avatar_img" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface IProps {
+    img?: string;
+    size?: 'sm' | 'md' | 'lg'
+}
+const props = defineProps<IProps>()
+const img = computed(() => props.img || 'src/assets/img/no-avatar.png')
+const size = computed(() => props.size || 'sm')
+
+</script>
+
+<style lang="scss" scoped>
+@import url('@/assets/styles/dls/_avatar.scss');
+</style>
