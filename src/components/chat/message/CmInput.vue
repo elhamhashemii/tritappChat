aa958fcb6d0c90a18ad63b674a14d4a251851502
Elham <hashemi.elham22@gmail.com>
2023-08-29 01:25:37 +0330

    fix: alert added

--- src/components/chat/message/CmInput.vue
+++ src/components/chat/message/CmInput.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="bg-gray-200 px-3 py-2 flex items-stretch justify-end gap-x-1">
         <div>
-            <Btn icon="attach">
-                <PaperClipIcon class="text-gray-500 w-5" />
-            </Btn>
+            <CmUpload />
         </div>
         <div class="w-full">
             <TextField  placeholder="Type a message . . ." />
@@ -19,7 +17,7 @@
 <script setup lang="ts">
 import TextField from '../../dls/TextField.vue';
 import Btn from "@/components/dls/btn.vue"
-import { PaperClipIcon } from '@heroicons/vue/24/outline'
 import { MicrophoneIcon } from '@heroicons/vue/24/outline'
+import CmUpload from './CmUpload.vue';
 
 </script>
---
+++ src/components/chat/message/CmUpload.vue
@@ -0,0 +1,13 @@
+<template>
+    <Btn @click="uploadFile" icon="attach">
+        <PaperClipIcon class="text-gray-500 w-5" />
+    </Btn>
+</template>
+
+<script setup lang="ts">
+import Btn from "@/components/dls/btn.vue"
+import { PaperClipIcon } from '@heroicons/vue/24/outline'
+function uploadFile() {
+    window.alert("I couldn't manage my time to add functionality to this btn, I'm sorry. 😥")
+}
+</script>
